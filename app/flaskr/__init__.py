3e4a16f593eb262bb18349dcc51be5b27103223e
Andrey Popov <ap4220@ic.ac.uk>
2022-06-07 18:18:59 +0100

    [feat/ap4220] connected and registered a db

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 
 def create_app(test_config=None):
+    ''' Application factory '''
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -28,5 +30,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
 
     return app
