d53faa12e06891124ae48fa8e7415397cfc29e43
Mike Sorokin <mike-sorokin@outlook.com>
2022-06-08 14:37:03 +0100

    Simple db test

---
+++ test/test_local_db_connection.py
@@ -0,0 +1,24 @@
+import pytest, psycopg2, os
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+
+def test_functional():
+    with conn.cursor() as curs:
+        curs.execute("""DROP TABLE IF EXISTS temp;""")
+        conn.commit()
+        curs.execute("""
+        CREATE TABLE temp (
+            Name TEXT,
+            Age INTEGER
+        );""")
+        conn.commit()
+        curs.execute("""
+        INSERT INTO temp
+        VALUES (\'Bob\', 42);""")
+        conn.commit()
+
+        curs.execute("""SELECT * FROM temp;""")
+        result = curs.fetchone()
+
+        assert result == ("Bob", 42)
