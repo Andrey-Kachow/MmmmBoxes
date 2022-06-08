92f4a260262ab774aa070038ba030a9cfe13f810
Mike Sorokin <mike-sorokin@outlook.com>
2022-06-08 11:39:10 +0100

    Attempt to setup postgressql with heroku

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
+import os, psycopg2
 from flask import Flask
 
 app = Flask(__name__)
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 @app.route("/")
 def index():
     return "<h1>Hello world!</h1>"
 
+
+
 if __name__ == "__main__":
+    # connect to postgresSQL db
+    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+    with conn.cursor() as curs:
+        curs.execute("""
+        CREATE TABLE test (
+            TestField int,
+            TestField1 varchar(255)
+        );
+        """)
+
+        curs.execute("""
+        INSERT INTO test (TestField, TestField1)
+        VALUES(1, \'hello world\')
+        """)
+
     app.debug = True
     app.run()
