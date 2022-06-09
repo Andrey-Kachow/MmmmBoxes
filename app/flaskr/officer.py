75289416741970f0e5e8f3c4ccb71a7487a816b7
Mike Sorokin <mike-sorokin@outlook.com>
2022-06-09 18:36:19 +0100

    Fixed git 'typo'

---
+++ app/flaskr/officer.py
@@ -0,0 +1,31 @@
+import functools
+from .database import db
+
+from flask import *
+from werkzeug.security import check_password_hash, generate_password_hash
+
+bp = Blueprint("officer", __name__, url_prefix="/officer")
+
+# Check user is logged in and is resident
+@bp.before_request
+def before_request():
+    if "user_id" not in session:
+        abort(403, "You are not logged in")
+    if not session["user_is_officer"]:
+        abort(403, "You are not a resident")
+
+@bp.context_processor
+def utility_processor():
+
+    def get_package_list():
+        return db.get_all_packages(current_app.db_conn)
+
+    return dict(get_package_list=get_package_list)
+
+@bp.route("/overview", methods=["GET", "POST"])
+def overview():
+    if request.method == "GET":
+        return render_template("officer/overview.html")
+    else:
+        db.add_new_package(current_app.db_conn, request.form["resident_name"], request.form["package_title"])
+        return render_template("officer/overview.html")
---
+++ app/flaskr/resident.py
@@ -0,0 +1,26 @@
+import functools
+from .database import db
+from flask import *
+from werkzeug.security import check_password_hash, generate_password_hash
+
+bp = Blueprint("resident", __name__, url_prefix="/resident")
+
+# Check user is logged in and is resident
+@bp.before_request
+def before_request():
+    if "user_id" not in session:
+        abort(403, "You are not logged in")
+    if session["user_is_officer"]:
+        abort(403, "You are not a resident")
+
+@bp.context_processor
+def utility_processor():
+
+    def get_package_list():
+        return db.get_all_packages(current_app.db_conn, session["user_id"])
+
+    return dict(get_package_list=get_package_list)
+
+@bp.route("/overview", methods=["GET"])
+def overview():
+    return render_template("resident/overview.html")
