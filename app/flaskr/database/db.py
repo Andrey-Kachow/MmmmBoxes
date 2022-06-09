73e5b22059d4e583617430b307d278a16cceb174
Andrey Popov <ap4220@ic.ac.uk>
2022-06-09 21:36:50 +0100

    [feat/ap4220] Connected local postgress db for local testing. also replaced deprecated alias

--- app/flaskr/database/db.py
+++ app/flaskr/database/db.py
@@ -6,17 +6,29 @@ def initialise_db_connection():
     """
     Returns: a psycopg2 database connection.
 
+    If local_db.json is found, returns a connection with local postgres database
     If creds.json is found, returns a connection with those credentials.
     Otherwise, attempts to connect to URL given by DATABASE_URL environment variable."""
+
+    try:
+        with open(os.path.join(os.path.dirname(__file__),"local_db.json")) as f:
+            conn_details = json.loads(f.read())
+            return psycopg2.connect(
+                    host=conn_details["host"],
+                    dbname=conn_details["dbname"],
+                    user=conn_details["user"],
+                    password=conn_details["password"]
+                )
+    except FileNotFoundError as e:
+        print("Local postgress database is not going to be used. Looking for credentials...")
+
     # Attempt to get creds
     user_creds = None
     try:
         with open(os.path.join(os.path.dirname(__file__),"creds.json")) as f:
             user_creds = json.loads(f.read())
             # Check that the relevant keys are in creds.json.
-            if all(cred_key in user_creds for cred_key in ["host", "database", "user" ,"password"]):
-                pass
-            else:
+            if not all(cred_key in user_creds for cred_key in ["host", "database", "user" ,"password"]):
                 print("Credentials are incomplete. Using DATABASE_URL variable.")
                 user_creds = None
 
@@ -29,13 +41,14 @@ def initialise_db_connection():
         return psycopg2.connect(
             os.environ["DATABASE_URL"],
             sslmode="require")
-    else:
-        return psycopg2.connect(
-                host=user_creds["host"],
-                database=user_creds["database"],
-                user=user_creds["user"],
-                password=user_creds["password"]
-            )
+
+    return psycopg2.connect(
+            host=user_creds["host"],
+            dbname=user_creds["database"],
+            user=user_creds["user"],
+            password=user_creds["password"]
+        )
+
 
 def execute_sql_file(conn, filename):
     """Arguments: a database connection, sql file in same directory as db.
