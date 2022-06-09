c517f217a1b115ef142c43ac7c60f48c78e410bc
Mike Sorokin <mike-sorokin@outlook.com>
2022-06-09 22:40:09 +0100

    Hotfix for heroku

--- runme.py
+++ runme.py
@@ -1,6 +1,11 @@
 from app import create_app, socketio
+from sys import argv
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app)
+    # Arguments have been passed in, so use them
+    if argv[1:]:
+        socketio.run(app, host=argv[1], port=argv[2])
+    else:
+        socketio.run(app)
