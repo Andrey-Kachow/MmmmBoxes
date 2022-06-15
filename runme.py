5b8cffc166593de178e8db7a101c5de6577f236f
Mike Sorokin <mike-sorokin@outlook.com>
2022-06-15 16:31:42 +0100

    Finally fixed fixed background colour change

--- runme.py
+++ runme.py
@@ -1,8 +1,21 @@
 from app import create_app, socketio
 from sys import argv
-from os import environ
+from os import environ, path
+from re import sub, DOTALL
 
 app = create_app()
+
+def replace_bg_colour(col):
+    # Replace app/static/style.css background colour
+    contents = None
+    with open(path.join(path.dirname(__file__), "app/static/style.css")) as f:
+        contents = f.read()
+    with open(path.join(path.dirname(__file__), "app/static/style.css"), "w") as f:
+        f.write(sub(r"(html.*background:)[^;]*(;.*)",
+                        r"\1"+col+r"\2",
+                        contents,
+                        flags=DOTALL
+                        ))
 
 if __name__ == "__main__":
     if not "FLASK_ENV" in environ:
@@ -12,8 +25,22 @@ if __name__ == "__main__":
         print("FLASK_APP is not set! Terminating.")
         exit()
 
-    # Arguments have been passed in, so use them
-    if argv[1:]:
-        socketio.run(app, host=argv[1], port=argv[2])
-    else:
+    # Check first argument to decide if running locally
+    env = argv[1]
+
+    if env == "live":
+        print("Running live.")
+        socketio.run(app, host=argv[2], port=argv[3])
+    elif env == "dev":
+        print("Running dev.")
+        replace_bg_colour("#ff8484")
+        socketio.run(app, host=argv[2], port=argv[3])
+    elif env == "local":
+        replace_bg_colour("#ff8484")
+
+        print("Running locally.")
         socketio.run(app)
+
+    else:
+        print("Incorrect live/dev/local flag")
+        exit()
