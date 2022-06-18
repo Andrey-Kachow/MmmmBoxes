75a22240473471844144e737f5c4e288245d365f
Mike Sorokin <mike-sorokin@outlook.com>
2022-06-18 12:47:05 +0100

    Test commit

--- app/main/populate_template.py
+++ app/main/populate_template.py
@@ -11,9 +11,11 @@ def replace(text: str, pattern: str, new_text: str) -> str:
     return re.sub(pattern, new_text, text)
 
 
-def personalise_email(email: str, full_name="", date_d="", date_t="", urgency="",
-                      description="") -> str:
+def personalise_email(
+    email: str, full_name="", date_d="", date_t="", urgency="", description=""
+) -> str:
     p_email = email
+
     p_email = replace(p_email, reg_fullname, full_name)
     p_email = replace(p_email, reg_date_d, date_d)
     p_email = replace(p_email, reg_date_t, date_t)
